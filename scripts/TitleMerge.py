d4a7d205abde54055d4a013e2d31a5dc3075ceee
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2023-01-21 23:11:58 -0500

    Account for paired titles

--- scripts/TitleMerge.py
+++ scripts/TitleMerge.py
@@ -27,22 +27,34 @@ AdjStrings = dict()
 SubStrings = dict()
 
 
-# all IDs have this stupid XXXX_0 or XXXX_1 formatting, where _1 indicates some weird parameter set:
+# all IDs have this stupid XXXX_0 or XXXX_1 formatting, where _1 indicates paired (eg. king/queen) data OR some weird parameter set:
 # [group=0001 type=0002 params=]
 # trim IDs to XXXX, and filter out parameter entries
 
 for item in AdjStringsRaw.items():
-    if not (len(item[0]) == 6 and item[0][4:] == "_1"):
-        AdjStrings[item[0][:4]] = item[1]
+    if item[1] != "[group=0001 type=0002 params=]":
+        if item[0][4:] == "_1":
+            # this is a paired title, we need to merge it with the last one (XXXX_0)
+            firstVariant = AdjStrings[item[0][:4]]
+            secondVariant = item[1]
+            AdjStrings[item[0][:4]] = f"{firstVariant} // {secondVariant}"
+        else:
+            #unpaired title
+            AdjStrings[item[0][:4]] = item[1]
 
 for item in SubStringsRaw.items():
-    if not (len(item[0]) == 6 and item[0][4:] == "_1"):
-        SubStrings[item[0][:4]] = item[1]
+    if item[1] != "[group=0001 type=0002 params=]":
+        if item[0][4:] == "_1":
+            firstVariant = SubStrings[item[0][:4]]
+            secondVariant = item[1]
+            SubStrings[item[0][:4]] = f"{firstVariant} // {secondVariant}"
+        else:
+            SubStrings[item[0][:4]] = item[1]
 
 
 # clean up
 
-del AdjStringsRaw, SubStringsRaw, StringData
+#del AdjStringsRaw, SubStringsRaw, StringData
 
 
 # pair strings and data, then merge into one list for each
@@ -78,3 +90,6 @@ with open("assets/titles/TitleAdjectiveInfo.json", 'w') as file:
 with open("assets/titles/TitleSubjectInfo.json", 'w') as file:
     file.write(json.dumps(SubData, indent=4))
     file.close()
+
+print(len(SubStringsRaw), len(SubStrings), len(SubData))
+print(len(AdjStringsRaw), len(AdjStrings), len(AdjData))

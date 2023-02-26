10731d203da8476aebe918e319b71d17e3e95d41
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2023-02-25 22:53:19 -0500

    Add primitive firebase test page

---
+++ js/firebasetest.js
@@ -0,0 +1,13 @@
+
+$(function() {
+    console.log("loaded!");
+
+    $(function(){
+        $("#nav-script").load("navbar.html")
+    });
+    
+    $(function(){
+        $("#footer-script").load("footer.html")
+    });
+    
+});

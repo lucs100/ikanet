ba8dc8d5dfb45c8d6bbabdb2f5c1faa829104c5d
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2023-03-10 15:17:34 -0500

    Implemented primitive localstorage

--- js/localstorage.js
+++ js/localstorage.js
@@ -2,4 +2,26 @@
 
 import { loadBookends } from "./functions/loadFrame.js";
 
+function pushToLocal() {
+    var textBox = document.getElementById("local-storage-test-text")
+    var data = textBox.value;
+    if (data != "") {
+        localStorage.setItem("testData", data);
+        console.log("Successfully stored " + localStorage.getItem("testData") + "!");
+        textBox.value = "";
+    }
+}
+
+function getFromLocal() {
+    var data = localStorage.getItem("testData");
+    if (data != "") {
+        var textBox = document.getElementById("output");
+        textBox.innerHTML = data;
+        console.log("Successfully retrieved " + data + "!");
+    }
+}
+
+document.getElementById("local-storage-test-submit").addEventListener("click", pushToLocal)
+document.getElementById("local-storage-test-recall").addEventListener("click", getFromLocal)
+
 loadBookends();
